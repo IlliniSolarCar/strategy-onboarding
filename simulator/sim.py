deed1e7e8c900a270e4971c14a951c6c0e81b983
Christopher Xu <christopher.y.x@gmail.com>
2022-09-09 00:34:57 -0500

    bettter print messages in sim.py

--- simulator/sim.py
+++ simulator/sim.py
@@ -5,22 +5,23 @@ from route.route import Route
 import numpy as np
 
 def main():
-    env = RaceEnv(do_render=True, do_print=False, pause_time=0)
+    env = RaceEnv(do_render=False, do_print=False, pause_time=0)
 
     env.set_try_loop(True)
 
     while True:
         
-        # env.set_target_mph(np.random.randint(30, 40))
+        env.set_target_mph(35)
         done = env.step()
         
         if done:
-            print(env.get_average_mph(), env.get_watthours(), env.get_time(), env.get_miles_earned())
-            print(env.get_legs_attempted())
-            print(env.get_legs_completed())
+            print(f"Miles earned: {env.get_miles_earned()}")
+            print(f"Average mph: {env.get_average_mph()}")
+            print(f"Energy left: {env.get_watthours()}")
+            print(f"Legs attempted: {env.get_legs_attempted()}")
+            print(f"Legs completed: {env.get_legs_completed()}")
             env.reset()
             break #if you only want to run the simulation once
-    
 
 if __name__ == "__main__":
     main()
